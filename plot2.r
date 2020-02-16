23320d7d67356f3d266a2a5737f06862e3eeabeb
Tochi_Okeke <32921464+Tocheee@users.noreply.github.com>
2020-02-15 23:53:40 -0800

    added plot2.r file

--- plot2.r
+++ plot2.r
@@ -1,15 +1,19 @@
 #Tocheee
 #Data Visualization Project 1
 #Read in the data
-#install.packages("sqldf")
+install.packages("sqldf")
 library(sqldf)
 library(lubridate)
-data <- read.csv.sql(file="household_power_consumption.txt",header=TRUE,sql='select * from file where Date = "1/2/2007" or Date = "2/2/2007"', sep=";")
+
+#Set working directory
+# setwd("~/R/Coursera/datavizprojecta/ExData_Plotting1")
+
+data <- read.csv.sql(file="household_power_consumption.csv",header=TRUE,sql='select * from file where Date = "1/2/2007" or Date = "2/2/2007"', sep=";")
 
 #Change date/time column to datetime class
 datetime <-  paste(data$Date,data$Time)
 data$Date <- strptime(datetime,format="%d/%m/%Y %H:%M:%S")
 
 png('plot2.png')
-png2 <- plot(x=(data$Date),y=data$Global_active_power,col="black", ylab = "Global Active Power (kilowatts)", xlab="", type='l')
+png2 <- plot(x=data$Date,y=data$Global_active_power,col="black", ylab = "Global Active Power (kilowatts)", type='l')
 dev.off()
